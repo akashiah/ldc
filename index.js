c9bc6a66b58bb8b03072f452eebdca824bdccc37
lionel_kg <lionel.komguem@inolians.com>
2025-06-08 01:49:26 +0200

    revert isInTab method

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 let tab = [3, 100, 8, 350, 20];
 
-function isInTab(valeur){
-    if ( tab.includes(valeur)) {
-        console.log("true")
-    }
-    else {console.log("false")}
-
+function isInTab(valeur) {
+  if (!tab.includes(valeur)) {
+    console.log("false");
+  } else {
+    console.log("true");
+  }
 }
 
-isInTab(
-    100
-)
+isInTab(100);
 
-function onflick(valeur){
-    console.log(tab.includes(valeur))
-
+function onflick(valeur) {
+  console.log(tab.includes(valeur));
 }
-onflick(
-    350
-)
+onflick(350);
